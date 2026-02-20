807429242316f710f8b9c5f5704b2b4b66687639
Abhishek <abhishek3182002@gmail.com>
2026-02-20 08:38:18 +0530

    Add gunicorn, whitenoise, and requirements updates

--- practice/settings.py
+++ practice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k@9@t+z1kk8op_#jezhm2v2=qe2f%dm3i_e-x$^d+--wz151qu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
